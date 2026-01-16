e9b773e6e36ae68a0fdbc6c803e7db9a4bd0f62f
IsuCake <61488220+IsuCake@users.noreply.github.com>
2026-01-17 02:41:17 +1100

    Add CallFunction, StartProcess & Else

--- .pyreutils/pyreutils/wrapper/__init__.py
+++ .pyreutils/pyreutils/wrapper/__init__.py
@@ -1,56 +1,149 @@
 from typing import TYPE_CHECKING
-from dfpyre import Function as DFFunction
-from dfpyre import Process as DFProcess
+
+# 1. IMPORTS: Raw classes aliased with '_' to hide them
+from dfpyre import Function as _Function
+from dfpyre import Process as _Process
+from dfpyre import CallFunction as _CallFunction
+from dfpyre import StartProcess as _StartProcess
+from dfpyre import Else as _Else
 from dfpyre import PlayerEvent as _PlayerEvent
 from dfpyre import EntityEvent as _EntityEvent
+from dfpyre import PlayerAction as _PlayerAction
+from dfpyre import GameAction as _GameAction
+from dfpyre import EntityAction as _EntityAction
+from dfpyre import SelectObject as _SelectObject
+from dfpyre import IfPlayer as _IfPlayer
+from dfpyre import IfVariable as _IfVariable
+from dfpyre import IfGame as _IfGame
+from dfpyre import IfEntity as _IfEntity
+from dfpyre import Repeat as _Repeat
 from dfpyre import *
 
+# Import Custom SetVariable Subclass
 from ..alias.SetVariable import SetVariable as _SetVariable
 from ..alias import *
 
-from .context import ContextBlock
+from .context import ContextBlock, _get_current_context
 from .emitter import AutoEmitter, HeaderEmitter
 from .smart_wrapper import SmartSetVariableWrapper
 from .magic_handler import MagicVarHandler
 from .math_ops import MathOp 
 
-if TYPE_CHECKING:
-    SetVariable = _SetVariable
-    PlayerAction = PlayerAction
-    IfPlayer = IfPlayer
-    GameAction = GameAction
-    IfGame = IfGame
-    EntityAction = EntityAction
-    IfEntity = IfEntity
-    SelectObject = SelectObject
-    Repeat = Repeat
+# ==========================================
+# 2. WRAPPER FUNCTIONS (Internal)
+# ==========================================
+
+def _Function_Wrapper(*args, **kwargs):
+    """Wraps Function() in a ContextBlock."""
+    return ContextBlock(_Function(*args, **kwargs))
+
+def _Process_Wrapper(*args, **kwargs):
+    """Wraps Process() in a ContextBlock."""
+    return ContextBlock(_Process(*args, **kwargs))
+
+def _CallFunction_Wrapper(*args, **kwargs):
+    """Wraps CallFunction with MathOp support."""
+    # 2. Create Block
+    block = _CallFunction(*args, **kwargs)
     
-    # Events (Type Checking)
+    # 3. Add to Context
+    ctx = _get_current_context()
+    if ctx is not None:
+        if isinstance(block, list): 
+            ctx.extend(block)
+        else: 
+            ctx.append(block)
+        
+    return block
+
+def _StartProcess_Wrapper(*args, **kwargs):
+    """Wraps StartProcess with MathOp support."""
+    block = _StartProcess(*args, **kwargs)
+    
+    ctx = _get_current_context()
+    if ctx is not None:
+        if isinstance(block, list): 
+            ctx.extend(block)
+        else: 
+            ctx.append(block)
+        
+    return block
+
+def _Else_Wrapper(*args, **kwargs):
+    """Wraps Else to handle bracket context."""
+    result = _Else(*args, **kwargs)
+    
+    # Handle Container Logic (Else closes previous block, opens new one)
+    if isinstance(result, list) and len(result) >= 2:
+        close_block = result[-1]      
+        immediate_blocks = result[:-1] 
+        
+        ctx = _get_current_context()
+        if ctx is not None: 
+            ctx.extend(immediate_blocks)
+            
+        return ContextBlock(immediate_blocks[0], close_block=close_block)
+    
+    # Fallback
+    ctx = _get_current_context()
+    if ctx is not None:
+        if isinstance(result, list): 
+            ctx.extend(result)
+        else: 
+            ctx.append(result)
+    return ContextBlock(result)
+
+if TYPE_CHECKING:
+    # --- IDE / STATIC ANALYSIS VIEW ---
+    # The IDE sees the ORIGINAL classes.
+    # This enables Auto-Complete and Ctrl+Click navigation to the library.
+    SetVariable = _SetVariable
+    PlayerAction = _PlayerAction
+    IfPlayer = _IfPlayer
+    GameAction = _GameAction
+    IfGame = _IfGame
+    EntityAction = _EntityAction
+    IfEntity = _IfEntity
+    SelectObject = _SelectObject
+    Repeat = _Repeat
+    
+    # Events
     PlayerEvent = _PlayerEvent
     EntityEvent = _EntityEvent
-else:
-    SetVariable = SmartSetVariableWrapper()
-    PlayerAction = AutoEmitter(PlayerAction)
-    GameAction = AutoEmitter(GameAction)
-    EntityAction = AutoEmitter(EntityAction)
-    SelectObject = AutoEmitter(SelectObject)
-    IfPlayer = AutoEmitter(IfPlayer)
-    IfVariable = AutoEmitter(IfVariable)
-    IfGame = AutoEmitter(IfGame)
-    IfEntity = AutoEmitter(IfEntity)
-    Repeat = AutoEmitter(Repeat)
     
-    # Wrap Events with HeaderEmitter
+    # Functions
+    CallFunction = _CallFunction
+    StartProcess = _StartProcess
+    Else = _Else
+    Process = _Process
+    Function = _Function
+
+else:
+    # --- RUNTIME VIEW ---
+    # Python executes this. It sees the WRAPPERS.
+    
+    # 1. Action Containers (AutoEmitter)
+    PlayerAction = AutoEmitter(_PlayerAction)
+    GameAction = AutoEmitter(_GameAction)
+    EntityAction = AutoEmitter(_EntityAction)
+    SelectObject = AutoEmitter(_SelectObject)
+    IfPlayer = AutoEmitter(_IfPlayer)
+    IfVariable = AutoEmitter(_IfVariable)
+    IfGame = AutoEmitter(_IfGame)
+    IfEntity = AutoEmitter(_IfEntity)
+    Repeat = AutoEmitter(_Repeat)
+    
+    # 2. Smart Variables
+    SetVariable = SmartSetVariableWrapper()
+
+    # 3. Events (HeaderEmitter)
     PlayerEvent = HeaderEmitter(_PlayerEvent)
     EntityEvent = HeaderEmitter(_EntityEvent)
-
-# Wrapper for Function
-def Function(*args, **kwargs):
-    block = DFFunction(*args, **kwargs)
-    return ContextBlock(block)
-
-# Wrapper for Process
-def Process(*args, **kwargs):
-    block = DFProcess(*args, **kwargs)
-    return ContextBlock(block)
-
+    
+    # 4. Standalone Wrappers (Explicit Assignment)
+    # This is where we force Python to use our logic.
+    CallFunction = _CallFunction_Wrapper
+    StartProcess = _StartProcess_Wrapper
+    Else = _Else_Wrapper
+    Function = _Function_Wrapper
+    Process = _Process_Wrapper
